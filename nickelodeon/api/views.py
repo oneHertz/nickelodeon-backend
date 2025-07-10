e463ffa18fb641bc9c2bbda6a5d70836881517a5
Raphael Stefanini <raphael@rphlo.com>
2025-07-10 18:27:32 +0300

    test

--- nickelodeon/api/views.py
+++ nickelodeon/api/views.py
@@ -174,7 +174,7 @@ class TextSearchApiView(generics.ListAPIView):
                     settings.DATABASES["default"]["ENGINE"]
                     == "django.db.backends.postgresql"
                 ):
-                    key = "filename__unaccent__icontains__search"
+                    key = "filename__unaccent__search"
                 else:
                     key = "filename__icontains"
                 query &= Q(**{key: search_term})

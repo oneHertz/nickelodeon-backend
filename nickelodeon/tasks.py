4f812b3c351c768b476049d12e7ed42218ed20bb
Raphael Stefanini <raphael@rphlo.com>
2025-07-11 14:13:22 +0300

    time

--- nickelodeon/tasks.py
+++ nickelodeon/tasks.py
@@ -52,7 +52,6 @@ def create_aac(mp3_id=""):
         return
     if not song.has_aac and song.has_mp3:
         mp3_path = song.get_file_format_path("mp3")
-        aac_path = song.get_file_format_path("aac")
         mp3_url = s3_object_url(mp3_path)
         with tempfile.NamedTemporaryFile() as aac_file:
             aac_tmp_path = aac_file.name
@@ -60,6 +59,8 @@ def create_aac(mp3_id=""):
             mp3_url,
             output_file_aac=aac_tmp_path,
         )
+        song.refresh_from_db()
+        aac_path = song.get_file_format_path("aac")
         with open(aac_tmp_path, mode="rb") as f:
             s3_upload(f, aac_path)
     if not song.aac:

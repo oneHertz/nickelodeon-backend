90a2633aa75110fe5f163c028877e4ae4e5831e2
Raphael Stefanini <raphael@rphlo.com>
2025-07-29 13:22:37 +0300

    Transcoding

--- nickelodeon/utils.py
+++ nickelodeon/utils.py
@@ -220,7 +220,7 @@ class FFMPEGTask(object):
 
 
 def transcode_audio(input_file, callback=None):
-    command = ["ffmpeg", "-y", "-i", input_file, "-threads", "0", "-vn"]
+    command = ["/usr/bin/ffmpeg", "-y", "-i", input_file, "-threads", "0", "-vn"]
     command += [
             "-ar",
             "44100",
